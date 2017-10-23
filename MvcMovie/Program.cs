24ae46a2474b7eb617a3dd169e33ae8056ed31cd
ruben rodrigues <ruben_7_benfica@hotmail.com>
2017-10-23 21:17:15 +0100

    adicionado SeedDatabase

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,21 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+using System;
 
 namespace MvcMovie
 {
     public class Program
     {
-        //inicio tutorial
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -24,3 +38,4 @@ namespace MvcMovie
                 .Build();
     }
 }
+
